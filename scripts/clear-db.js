2bfe43947568a83224290ee951fe2471cc9ab1d1
xiechao06 <xiechao06@gmail.com>
2015-09-16 16:50:58 +0800

    fix #1

--- scripts/clear-db.js
+++ scripts/clear-db.js
@@ -1,14 +1,19 @@
+#! /usr/bin/env node
 var fs = require('fs');
 var q = require('q');
 
-function clearDB() {
+function clearDB(knex) {
     return q.nfcall(fs.unlink, './db');
 }
 module.exports = clearDB;
 
 if (require.main === module) {
-    var knex = require('../setup-knex.js');
-    clearDB(knex).done(function () {
-        knex.destroy();
+    var unlink = q.denodeify(fs.unlink);
+    clearDB().catch(function (err) {
+        if (err.code != 'ENOENT') {
+            throw err; 
+        }
+        console.log('no such db');
+    }).done(function () {
     });
 }
