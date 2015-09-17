a19aa435c6e5f2182f27e29898cd25d627cba85d
xiechao06 <xiechao06@gmail.com>
2015-09-17 15:24:23 +0800

    fix #9

--- scripts/make-test-data.js
+++ scripts/make-test-data.js
@@ -9,12 +9,29 @@ var _ = require('underscore');
 var chance = require('chance')();
 
 initDB(knex).then(function () {
-    return knex.insert(_(32).times(function () {
+    return knex.insert([
+        '市政基础设施建设',
+        '文教卫',
+        '政法',
+        '水利',
+        '交通'
+    ].map(function (name) {
+        return {
+            name: name,
+            description: chance.sentence(),
+            created_at: new Date(),
+        };
+    })).into('TB_PROJECT_TYPE');
+}).then(function () {
+    return knex('TB_PROJECT_TYPE').select('*');
+}).then(function (types) {
+    return knex.insert(_(64).times(function () {
         return {
             name: chance.word(),
             budget: chance.natural(),
             description: chance.sentence(),
             created_at: new Date(),
+            project_type_id: _.sample(types).id,
         };
     })).into('TB_PROJECT');
 }).done(function () {
