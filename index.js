b76723116780dba2d2c3d086206e8d9b19d5faaa
xiechao06 <xiechao06@gmail.com>
2015-09-18 13:44:04 +0800

    fix #12

--- index.js
+++ index.js
@@ -3,57 +3,35 @@ var json = require('koa-json');
 var cors = require('kcors');
 var knex = require('./setup-knex.js');
 var bunyan = require('bunyan');
+var koaBody = require('koa-body')();
 
 var app = koa();
 var router = require('koa-router')();
 
 var logger = bunyan.createLogger({name: "ppp-lite"});
 var koaLogger = require('koa-bunyan');
+var models = require('./models.js');
 
 router.get('/project/list.json', function *(next) {
-    var q = knex('TB_PROJECT').count('* as count');
-    var totalCount = (yield q)[0].count;
+    var totalCount = yield models.Project.count();
+    var model = models.Project;
     
-    q = knex('TB_PROJECT as p').select(
-        'p.id as _id',
-        'p.name as _name',
-        'p.budget as _budget',
-        'p.description as _description',
-        'p.created_at as _created_at',
-        'p.updated_at as _updated_at',
-        'pt.id as pt_id',
-        'pt.name as pt_name',
-        'pt.description as pt_description',
-        'pt.created_at as pt_created_at',
-        'pt.updated_at as pt_updated_at'
-    ).join('TB_PROJECT_TYPE as pt', 'p.project_type_id', 'pt.id');
-
     if (this.query.page && this.query.per_page) {
         var page = parseInt(this.query.page);
         var perPage = parseInt(this.query.per_page);
-        q = q.offset((page - 1) * perPage).limit(perPage);
+        model = model.query(function (q) {
+            q.offset((page - 1) * perPage).limit(perPage);
+        });
     }
 
     this.body = {
-        data: yield q.map(function (row) {
-            return {
-                id: row._id,
-                name: row._name,
-                description: row._description,
-                createdAt: row._created_at,
-                updatedAt: row._updated_at,
-                budget: row._budget,
-                projectType: {
-                    id: row.pt_id, 
-                    name: row.pt_name,
-                    description: row.pt_description,
-                    createdAt: row.pt_created_at,
-                    updatedAt: row.pt_updated_at
-                }
-            };
-        }),
+        data: (yield model.fetchAll({ withRelated: ['projectType'] })).toJSON(),
         totalCount: totalCount,
     };
+    yield next;	
+}).post('/project/object', koaBody, function *(next) {
+    var model = yield models.Project.forge(this.request.body).save();
+    this.body = (yield model.load('projectType')).toJSON();
     yield next;	
 });
 
