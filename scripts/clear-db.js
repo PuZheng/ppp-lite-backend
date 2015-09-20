572934cf1b17b886ae221de432d776bb62ff4911
xiechao06 <xiechao06@gmail.com>
2015-09-20 12:50:16 +0800

    fix #10

--- scripts/clear-db.js
+++ scripts/clear-db.js
@@ -1,18 +1,18 @@
 #! /usr/bin/env node
 var fs = require('fs');
-var q = require('q');
 
 function clearDB(knex) {
-    return q.nfcall(fs.unlink, './db');
+    fs.unlink('./db', function (err) {
+        if (err) {
+            if (err.code != 'ENOENT') {
+                throw err; 
+            }
+            console.log('no such db');
+        }
+    });
 }
 module.exports = clearDB;
 
 if (require.main === module) {
-    clearDB().catch(function (err) {
-        if (err.code != 'ENOENT') {
-            throw err; 
-        }
-        console.log('no such db');
-    }).done(function () {
-    });
+    clearDB();
 }
--- scripts/make-test-data.js
+++ scripts/make-test-data.js
@@ -1,15 +1,17 @@
 #! /usr/bin/env node
 
 var knex = require('../setup-knex.js');
-var q = require('q');
+var co = require('co');
 var fs = require('fs');
 
 var initDB = require('./init-db.js');
-var _ = require('underscore');
+var _ = require('lodash');
 var chance = require('chance')();
 
-initDB(knex).then(function () {
-    return knex.insert([
+co(function *() {
+    yield initDB(knex);
+    console.log('creating project types...');
+    yield knex.insert([
         '市政基础设施建设',
         '文教卫',
         '政法',
@@ -22,10 +24,10 @@ initDB(knex).then(function () {
             created_at: new Date(),
         };
     })).into('TB_PROJECT_TYPE');
-}).then(function () {
-    return knex('TB_PROJECT_TYPE').select('*');
-}).then(function (types) {
-    return knex.insert(_(64).times(function () {
+
+    console.log('creating projects...');
+    var projectTypes = yield knex('TB_PROJECT_TYPE').select('*');
+    yield knex.insert(_(64).times(function () {
         return {
             name: chance.word(),
             budget: chance.natural({
@@ -34,9 +36,44 @@ initDB(knex).then(function () {
             }),
             description: chance.sentence(),
             created_at: new Date(),
-            project_type_id: _.sample(types).id,
+            project_type_id: _.sample(projectTypes).id,
         };
-    })).into('TB_PROJECT');
-}).done(function () {
+    }).value()).into('TB_PROJECT');
+
+    console.log('creating tags...');
+    yield knex.insert([
+        '建设',
+        '水利',
+        '教育',
+        '卫生',
+        'BT',
+        'BOT',
+        'BOOT',
+        'TOT',
+        '道路',
+        '固废垃圾'
+    ].map(function (tag) {
+        return { value: tag };
+    })).into('TB_TAG');
+
+    var projects = yield knex('TB_PROJECT').select('*');
+    var tags = yield knex('TB_TAG').select('*');
+    
+    var data = _.flatten(projects.map(function (project) {
+        return  _(_.random(1, 5)).times(
+            function () {
+                return {
+                    project_id: project.id,
+                    tag_id: _.sample(tags).id,
+                };
+            }
+        ).value();
+    }));
+    yield knex.insert(data).into('TB_PROJECT_TAG');
+}).then(function () {
     knex.destroy();
+    console.log('make test data done!');
+}, function (err) {
+    knex.destroy();
+    console.log(err);
 });
